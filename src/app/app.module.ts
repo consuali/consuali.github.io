1bbaebc6710ac87f32c7586fc4e5a05ce7706991
Consuelo <consuelo.fadda@synthesis3.it>
2023-10-07 21:11:46 +0200

    add component and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { IonicModule } from '@ionic/angular';
+import { IonIcon, IonicModule, Platform } from '@ionic/angular';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 const routes: Routes = [];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    NavbarComponent,
   ],
   imports: [
-    BrowserModule, RouterModule.forRoot(routes),
-    IonicModule.forRoot(),
-  ], exports: [RouterModule],
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    IonicModule.forRoot(), BrowserAnimationsModule
+
+  ], exports: [RouterModule, IonicModule],
   providers: [],
   bootstrap: [AppComponent]
 })
